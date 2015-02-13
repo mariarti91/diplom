f2d6f3eb230d8e526e4fcdbdd770ad3eaadc2ff0
MJ <nds@keva.tusur.ru>
2015-02-13 17:09:22 +0600

    added Client Socket

---
+++ server/src/clientsock.cpp
@@ -0,0 +1,84 @@
+#include "clientsock.h"
+
+ClientSock::ClientSock(QObject *parent) :
+    QObject(parent)
+{
+    sc = new QTcpSocket(this);
+}
+//--------------------------------------------------------------------
+
+ClientSock::ClientSock(QString addr, int port, QObject *parent) :
+    QObject(parent)
+{
+    sc = new QTcpSocket(this);
+    initSc(addr, port);
+}
+//--------------------------------------------------------------------
+
+ClientSock::ClientSock(QTcpSocket *sock, QObject *parent) :
+    QObject(parent)
+{
+    sc = sock;
+    this->setName(QString("%1").arg(sock->peerPort()));
+    connect(sc, SIGNAL(readyRead()), this, SLOT(getData()));
+    startTimer(5000);
+}
+
+//--------------------------------------------------------------------
+
+ClientSock::~ClientSock()
+{
+    delete sc;
+}
+//--------------------------------------------------------------------
+
+void ClientSock::initSc(const QString &addr, const int &port)
+{
+    sc->connectToHost(addr, port);
+    connect(sc, SIGNAL(readyRead()), this, SLOT(getData()));
+}
+//--------------------------------------------------------------------
+
+QByteArray ClientSock::getData()
+{
+    QByteArray buf(sc->readAll());
+    std::cout << m_qsScName.toStdString() << ": " << QString(buf).toStdString() << std::endl;
+    return buf;
+}
+//--------------------------------------------------------------------
+
+void ClientSock::setName(const QString &name)
+{
+    m_qsScName = name;
+}
+//--------------------------------------------------------------------
+
+void ClientSock::sendData(QByteArray data)
+{
+    if(sc->state() == QAbstractSocket::ConnectedState)
+    {
+        sc->write(data);
+    }
+}
+//--------------------------------------------------------------------
+
+void ClientSock::timerEvent(QTimerEvent *event)
+{
+    if(sc->state() == QAbstractSocket::UnconnectedState)
+    {
+        emit scDisconnect(m_qsScName);
+        killTimer(event->timerId());
+        std::cout << m_qsScName.toStdString() << " status: CLOSED" << std::endl;
+    }
+    else
+    {
+        //std::cout << m_qsScName.toStdString() << " status: OK" << std::endl;
+    }
+}
+//--------------------------------------------------------------------
+
+QString ClientSock::getName()
+{
+    return m_qsScName;
+}
+//--------------------------------------------------------------------
---
+++ server/src/clientsock.h
@@ -0,0 +1,33 @@
+#ifndef CLIENTSOCK_H
+#define CLIENTSOCK_H
+
+#include <QObject>
+#include <QString>
+#include <QTcpSocket>
+#include <QTimer>
+#include <QTimerEvent>
+#include <iostream>
+
+class ClientSock : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ClientSock(QObject *parent = 0);
+    explicit ClientSock(QString addr, int port, QObject *parent = 0);
+    explicit ClientSock(QTcpSocket *sock, QObject *parent = 0);
+    ~ClientSock();
+    void initSc(const QString & addr, const int & port);
+    void setName(const QString & name);
+    QString getName();
+    void sendData(QByteArray data);
+    void timerEvent(QTimerEvent *event);
+private:
+    QTcpSocket * sc;
+    QString m_qsScName;
+signals:
+    void scDisconnect(QString);
+public slots:
+    QByteArray getData();
+};
+
+#endif // CLIENTSOCK_H
